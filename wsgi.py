40639755f488a65a1f57ebfef876893934353960
Miroslav Brabenec <brabemi@gmail.com>
2017-05-04 13:08:55 +0200

    gitignore cleanup

--- wsgi.py
+++
@@ -1,6 +0,0 @@
-from app import app as application, load_app_cfg
-
-if __name__ == "__main__":
-	#~ app_cfg = load_app_cfg()
-	#~ application.config['app_config'] = app_cfg
-	application.run(debug=True)
